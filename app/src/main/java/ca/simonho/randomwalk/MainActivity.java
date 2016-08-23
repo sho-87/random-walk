df46fdc5e9a45e2317422fb2179816aab2593def
Simon Ho <simonsays87@googlemail.com>
2016-08-22 20:40:03 -0700

    Remove unused views

--- app/src/main/java/ca/simonho/randomwalk/MainActivity.java
+++ app/src/main/java/ca/simonho/randomwalk/MainActivity.java
@@ -18,14 +18,6 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
@@ -43,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_about) {
             return true;
         }
 
